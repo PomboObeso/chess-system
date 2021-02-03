9853832549ce71d2c23ab5bcc58a4132867610af
PomboObeso <matheus.gomes201@gmail.com>
2021-02-02 21:50:11 -0300

    Boarding Exception and denfensive programming

---
+++ src/boardgame/BoardgameException.java
@@ -0,0 +1,10 @@
+package boardgame;
+
+public class BoardgameException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public BoardgameException(String msg) {
+		super(msg);
+	}
+}
