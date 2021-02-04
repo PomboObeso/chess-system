bd5a3f37e5bb5774f8f4af2a6d355574f6da26eb
PomboObeso <matheus.gomes201@gmail.com>
2021-02-03 22:00:45 -0300

    Moving Pieces

--- src/chess/ChessException.java
+++ src/chess/ChessException.java
@@ -1,6 +1,8 @@
 package chess;
 
-public class ChessException extends RuntimeException {
+import boardgame.BoardgameException;
+
+public class ChessException extends BoardgameException {
 	private static final long serialVersionUID = 1L;
 	
 	public ChessException(String msg) {
